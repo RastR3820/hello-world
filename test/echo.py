779e1cdebafdf44f8339e9abec79ad33445e183e
RastR3820 <RastR3820@uhcl.edu>
2020-10-21 12:36:46 -0500

    Created a new folder for an echo class and added function to main

---
+++ test/echo.py
@@ -0,0 +1,8 @@
+# This is a python file to test how class in different folders work and how git from command line works
+
+class Marco():
+	def __init__(self,msg):
+		self.msg = msg
+
+	def polo(self):
+		print(self.msg)
